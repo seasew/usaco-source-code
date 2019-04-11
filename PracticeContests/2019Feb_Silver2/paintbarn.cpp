f9d7f2a400f1c5785e48ec828e4536022d068a65
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-04-10 18:16:55 -0700

    paintbarn 1st implementation

--- PracticeContests/2019Feb_Silver2/paintbarn.cpp
+++ PracticeContests/2019Feb_Silver2/paintbarn.cpp
@@ -10,6 +10,8 @@ PROB: paintbarn
 #include <algorithm>
 #include <iterator>
 
+int barn[1000][1000];
+
 int main()
 {
 	// Open Streams
@@ -27,10 +29,34 @@ int main()
 	}
 
 	// Read File 
-	fin >> VARIABLE;
+	int n;
+	int k;
+	fin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		int x1, y1, x2, y2;
+		fin >> x1 >> y1 >> x2 >> y2;
+		for (int a = y1; a < y2; a++)
+		{
+			barn[x1][a]++;
+			barn[x2][a]--;
+		}
+	}
+
+	int x = 0;
+	for (int i = 0; i < 1000; i++)
+	{
+		for (int j = 0; j < 1000; j++)
+		{
+			if (barn[i][j] == k)
+			{
+				x++;
+			}
+		}
+	}
 
 	// Write to File
-	fout << "This is my OUTPUT!!!";
+	fout << x << "\n";
 
 	// Close Streams
 	fin.close();
