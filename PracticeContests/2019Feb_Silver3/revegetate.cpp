b125967e29500476354d13a0cf2a1aa2ef62a0aa
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-04-21 11:44:46 -0700

    Read file revegetate

--- PracticeContests/2019Feb_Silver3/revegetate.cpp
+++ PracticeContests/2019Feb_Silver3/revegetate.cpp
@@ -9,7 +9,19 @@ PROB: revegetate
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <vector>
 
+
+int n;
+int m;
+// if cannot find a solution
+bool impossible;
+// an array of arrays refering to each pastures' same pastures
+std::vector<int> same[1000000];
+// an array of arrays referring to each pastures' different pastures
+std::vector<int> diff[100000];
+// values of 0 if not visted yet, 1 or 2
+int pastures[100000];
 
 int main()
 {
@@ -28,6 +40,25 @@ int main()
 	}
 	
 	// read file
+	fin >> n >> m;
+	// for each cow
+	for (int i = 0; i < m; i++)
+	{
+		char c;
+		int p1, p2;
+		fin >> c >> p1 >> p2;
+		if (c == 'S')
+		{
+			// add the respective pastures to same array
+			same[p1 - 1].push_back(p2);
+			same[p2 - 1].push_back(p1);
+		}
+		else
+		{
+			diff[p1 - 1].push_back(p2);
+			diff[p2 - 1].push_back(p1);
+		}
+	}
 
 	// Write to File
 	fout << "";
