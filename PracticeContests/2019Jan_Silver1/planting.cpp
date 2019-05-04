0b0be4a10ef61978c642215b6387dc2fbc55b0d3
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-05-04 10:58:16 -0700

    New implementation

--- PracticeContests/2019Jan_Silver1/planting.cpp
+++ PracticeContests/2019Jan_Silver1/planting.cpp
@@ -11,77 +11,8 @@ PROB: planting
 #include <iterator>
 #include <vector>
 
-int n;
-std::vector<int> adjacent[100001];
-std::vector<int> nearAdjacent[100001];
-int fieldArr[100001];
-bool grassArr[100001];
-
-int maxgrass;
-
-void plant(int field, int grass)
-{
-	// both parameters are 1 based
-	// update fields array
-	fieldArr[field] = grass;
-
-	std::fill(grassArr, grassArr + n, false);
-
-	// update grassArr array with current adjacent grasses that have already been planted
-	for (int adjacentF : adjacent[field])
-	{
-		if (fieldArr[adjacentF])
-		{
-			grassArr[fieldArr[adjacentF]] = true;
-		}
-	}
-
-	// then update with newly planted one
-	grassArr[grass] = true;
-
-	// calculate correct grass
-	int newGrass;
-	for (int i = 1; i < 100001; i++)
-	{
-		if (!grassArr[i])
-		{
-			newGrass = i;
-			break;
-		}
-	}
-
-	// find adjacent fields that have not been planted yet
-	for (int adjacentF : adjacent[field])
-	{
-		// if zero
-		if (!fieldArr[adjacentF])
-		{
-			// update grassmax
-			maxgrass = std::max(maxgrass, newGrass);
-			// plant it with the smallest grass possible
-			plant(adjacentF, newGrass);
-
-			
-		}
-	}
-
-	// find near-adjacent fields that have not been planted yet
-	for (int nearAdjacentF : nearAdjacent[field])
-	{
-		// if zero
-		if (!fieldArr[nearAdjacentF])
-		{
-			// update grassmax
-			maxgrass = std::max(maxgrass, newGrass);
-
-			// plant with the smallest true grass
-			plant(nearAdjacentF, newGrass);
-		}
-	}
-
-
-
-}
+// field array to hold the number of adjacent fields for each field
+int fields[100001];
 
 int main()
 {
@@ -99,41 +30,22 @@ int main()
 		return 1;
 	}
 
-	// Read file
+	int n;
 	fin >> n;
-	for (int i = 0; i < n - 1; i++)
+
+
+	for (int i = 1; i < n; i++)
 	{
 		int a, b;
 		fin >> a >> b;
-		
-		// add adjacent field
-		adjacent[a].push_back(b);
-		adjacent[b].push_back(a);
+		// increment
+		fields[a]++;
+		fields[b]++;
 	}
 
-	// add the near adjacent fields
-	for (int i = 1; i <= n; i++)
-	{
-		// for all adjacent fields to i
-		for (int adjacentF : adjacent[i])
-		{
-			// use its adjacent fields (!=i) and add to nAdja
-			for (int adjacentF2 : adjacent[adjacentF])
-			{
-				if (adjacentF2 != i)
-				{
-					nearAdjacent[i].push_back(adjacentF2);
-				}
-			}
-		}
-	}
+	int x = *std::max_element(fields, fields + n);
 
-	maxgrass = 1;
-	// plant starting with field 1 on grass 1
-	plant(1, 1);
-
-	// write to file
-	fout << maxgrass << "\n";
+	fout << (x) + 1 << "\n";
 
 	// Close Streams
 	fin.close();
