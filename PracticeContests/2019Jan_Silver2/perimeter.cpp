d08b345af62af0eaa7529c6c65dce72d60e48282
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-05-23 17:03:29 -0700

    Test case 10

--- PracticeContests/2019Jan_Silver2/perimeter.cpp
+++ PracticeContests/2019Jan_Silver2/perimeter.cpp
@@ -93,7 +93,7 @@ int perimeter(int label)
 int main()
 {
 	// Open Streams
-	std::ifstream fin("perimeter.in");
+	std::ifstream fin("C:\\Users\\alina_000\\Pictures\\From Internet\\10.in");
 
 	std::ofstream fout("perimeter.out");
 
