a3ce67eeb3f83067d9fbbdb9c1fd952d04566429
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-06-04 09:45:57 -0700

    Initialize variables

--- PracticeContests/2018Dec_Silver2/convention2.cpp
+++ PracticeContests/2018Dec_Silver2/convention2.cpp
@@ -11,6 +11,17 @@ PROB: convention2
 #include <iterator>
 #include <vector>
 
+// num of cows
+int n;
+// the arrival times
+int arrivals[100050];
+// the time spent grazing (corresponds to the arrivals array
+int times[100050];
+// true if waiting, false if not (corresponds to the arrivals array)
+bool waiting_cows[100050];
+// senority array (corresponds to the arrivals array)
+int senority[100050];
+
 int main()
 {
 	// Open Streams
