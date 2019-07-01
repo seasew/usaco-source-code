9ff81dee97ec2d688d1659cb48d2cfbe40dff2ee
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-07-01 15:56:39 -0700

    Create mooyoo and read file into variables

---
+++ PracticeContests/2018Dec_Silver3/mooyomooyo.cpp
@@ -0,0 +1,49 @@
+/*
+ID: alina.l1
+LANG: C++
+PROB: mooyomooyo
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <functional>
+#include <queue>
+#include <utility>
+
+// number of rows, <= 100
+int n;
+// minimum number of haybales in a blob
+int k;
+// contains numbers 0-9 representing the board
+int board[10][150];
+
+int main()
+{
+	// Open Streams
+	std::ifstream fin("mooyomooyo.in");
+
+	std::ofstream fout("mooyomooyo.out");
+
+	// Check
+	if (!fin.is_open() || !fout.is_open())
+	{
+		std::cout << "File could not be opened.";
+		fin.close();
+		fout.close();
+		return 1;
+	}
+
+	// read file
+
+	// write to file
+
+	// Close Streams
+	fin.close();
+	fout.close();
+
+	return 0;
+}
