fb31fbaa621788e260839ed9d76de4353adeba04
pinkbluesky <46383286+pinkbluesky@users.noreply.github.com>
2019-07-09 15:14:38 -0700

    delete most of code

--- PracticeContests/2018Dec_Silver3/mooyomooyo.cpp
+++ PracticeContests/2018Dec_Silver3/mooyomooyo.cpp
@@ -25,127 +25,12 @@ int n;
 int k;
 // contains numbers 0-9 representing the board
 int board[10][101];
-bool checked[10][101] = { false };
-
-// array containing the positions of the current blob
-std::set<std::pair<int, int>> curblob;
 
 // recursive method to record the current blob (curblob should be empty when this method is called)
 void floodfill(int target, int i, int j)
 {
-	// check 1: i and j are in bounds
-	if (!(i >= 0 && i < n && j >= 0 && j < n))
-	{
-		return;
-	}
+	
 
-	// check 2: board[i][j] has color target
-	if (board[i][j] != target)
-	{
-		return;
-	}
-
-	// otherwise, continue
-
-	// add i,j to curblob
-	curblob.insert(std::make_pair(i, j));
-	// set its checked value to true
-	checked[i][j] = true;
-
-	// call floodfill for all four directions
-	for (int a = 0; a < 4; a++)
-	{
-		floodfill(target, i + deltai[a], j + deltaj[a]);
-	}
-}
-
-// turn blobs with color key and size at least k into zeros, return true if blobs found
-bool labelBlobs(int key)
-{      
-	// bool to return
-	bool hasblobs = false;
-
-	bool firstcheck = true;
-	curblob.clear();
-
-	for (int i = 0; i < 10; i++)
-	{
-		std::fill(checked[i], checked[i] + n, false);
-	}
-
-	// while there are still curblobs
-	while (curblob.size() > 0 || firstcheck)
-	{
-		firstcheck = false;
-		// floodfill the first key color found in board
-		// if key color not found, exit
-
-		int nexti, nextj = -1;
-
-		// for each column
-		for (int i = 0; i < 10; i++)
-		{
-			// find the first occurence of key color with the checked value false
-			bool continue_search = true;
-			while (continue_search)
-			{
-				auto it = std::find(std::begin(board[i]), std::end(board[i]), key);
-				int j = std::distance(std::begin(board[0]), it);
-
-				// stop the search when no more keys can be found
-				if (it == std::end(board[i]))
-				{
-					continue_search = false;
-				}
-				// stop the search when a key is found and unchecked
-				else if (it != std::end(board[i]) && !checked[i][j])
-				{
-					continue_search = false;
-					nexti = i;
-					nextj = j;
-				}
-			}
-
-			// if continue_search is false
-			if (!continue_search)
-			{
-				// exit the for loop
-				break;
-			}
-		}
-
-		// FLOODFILL if the curblob meets size requirements, starting with pos (nexti, nextj)
-		if (nexti >= 0 && nextj >= 0)
-		{
-			floodfill(key, nexti, nextj);
-
-			// if the curblob is large enough, color with 0s
-			if (curblob.size() >= k)
-			{
-				// color in the positions in curblob
-				auto setit = curblob.begin();
-				for (int a = 0; a < curblob.size(); a++)
-				{
-					std::pair<int, int> pos = *setit;
-
-					board[pos.first][pos.second] = 0;
-
-					// update iterator
-					setit++;
-				}
-
-				// set hasblobs to true
-				hasblobs = true;
-			}
-		}
-		// no nexti & nextj was found, exit immediately
-		else
-		{
-			break;
-		}
-	}
-
-	return hasblobs;
 }
 
 int main()
@@ -175,37 +60,7 @@ int main()
 		}
 	}
 
-	bool hasBlobs = true;
-
-	while (hasBlobs)
-	{
-		// turn blobs into zeros for each of the 9 colors
-		for (int i = 1; i <= 9; i++)
-		{
-			hasBlobs = labelBlobs(i);
-		}
-
-		// only continue if there are blobs, otherwise the while loop exits
-		if (hasBlobs)
-		{
-			// gravity
-			// for each of the 10 col, start at index [n-1]
-			// find haybales until the previous one is 0 but the current one is not
-			for (int i = 0; i < 10; i++)
-			{
-				int lasti = n - 1;
-				while (board[i][lasti] == 0)
-				{
-					lasti--;
-				}
-
-				// lasti is the last index of a colored hay bale
-				// remove all zeros in range 0 to lasti + 1
-				std::remove(std::begin(board), std::begin(board) + lasti + 1, 0);
-			}
-
-		}
-	}
+	
 
 	// write to file
 	for (int i = 0; i < 10; i++)
